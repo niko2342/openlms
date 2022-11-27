0303349a954354cd4f35c7149648c9280de4792b
Nico Wagner <nwagner84@protonmail.com>
2022-11-27 20:07:09 +0100

    Remove unique constraint on salt column

--- migrations/2022-11-27-101139_create_users/up.sql
+++ migrations/2022-11-27-101139_create_users/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "users" (
     id SERIAL PRIMARY KEY,
     username character varying(256) NOT NULL UNIQUE,
-    password_salt character (12) NOT NULL UNIQUE,
+    password_salt character (12) NOT NULL,
     password_hash character varying(256) NOT NULL,
     created timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL, 
     updated timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
